9cd95c277d2e6279463b054fb4b8f47defdecd68
Chua Chiah Soon <chiahsoon18@gmail.com>
2022-03-04 16:20:52 +0800

    feat: remove jobq and rely on timeout

--- handlers/monitor.go
+++ handlers/monitor.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net"
+	"time"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/chiahsoon/cz4013-client/api"
@@ -27,47 +28,51 @@ func HandleMonitor(action models.UserSelectedAction, conn *net.UDPConn) {
 	}
 	req.Data = input
 
-	// Blocks while monitoring
-	if err = listenForCallbacks(conn, req); err != nil {
+	// Initial request to start monitoring
+	codec := &codec.Codec{}
+	encoded, err := codec.Encode(req)
+	if err != nil {
 		services.PP.PrintError(err.Error(), "", "")
 		return
 	}
+
+	if err := services.ConnSvc.SendRequest(conn, encoded); err != nil {
+		services.PP.PrintError(err.Error(), "", "")
+		return
+	}
+
+	// Block while monitoring
+	intervalEnd := time.Now().Add(time.Duration(input.Interval) * time.Second)
+	if err = listenForCallbacks(conn, intervalEnd); err != nil {
+		services.PP.PrintError(err.Error(), "", "")
+		return
+	}
+
+	services.PP.PrintMessage("Ending interval ...", "", "")
 }
 
-func listenForCallbacks(conn *net.UDPConn, req api.Request) error {
-	// Initiate monitoring
-	c := codec.Codec{}
-	encoded, err := c.Encode(req)
-	if err != nil {
-		return err
-	}
+func listenForCallbacks(conn *net.UDPConn, intervalEnd time.Time) error {
+	defer conn.SetDeadline(time.Time{}) // Reset to no deadlines after
+	codec := codec.Codec{}
 
-	// Initial request to start monitoring
-	if err := services.ConnSvc.SendRequest(conn, encoded); err != nil {
-		return err
-	}
-
-	// Listen for update callbacks
-	for {
-		resp := apiModels.CallbackPayload{}
-		if err := services.ConnSvc.GetResponse(conn, &resp); err != nil {
+	for time.Now().Before(intervalEnd) {
+		conn.SetDeadline(intervalEnd)
+		resp := &api.Response{}
+		if err := services.ConnSvc.GetResponse(conn, resp); err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				return nil
+			}
 			return err
 		}
 
 		var respData string
-		if err := c.DecodeAsInterface(resp.Data, &respData); err != nil {
+		if err := codec.DecodeAsInterface(resp.Data, &respData); err != nil {
 			services.PP.PrintError(err.Error(), "", "")
 			continue
 		}
 
-		switch apiModels.CallbackFunctionId(resp.FunctionId) {
-		case apiModels.UpdateCallback:
-			services.PP.Print(respData, "- Updated Accounts -", "")
-		case apiModels.StopMonitoringCallback:
-			services.PP.PrintMessage(respData, "", "")
-			return nil
-		default:
-			services.PP.PrintError("Invalid Callback", "", "")
-		}
+		services.PP.Print(respData, "- Updated Accounts -", "")
 	}
+
+	return nil
 }
